068cd8c990b29f64d15bb0c057034d2293dd67c5
Aline Lemos <alinelemos@id.uff.br>
2024-09-17 21:40:01 -0300

    movie init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import{SidebarItem} from './components/Sidebar/sidebarIndex'
+
+
+function App() {
+
+  return (
+    <>
+     <SidebarItem/>
+    </>
+  )
+}
+
+export default App
